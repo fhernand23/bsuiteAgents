db39c9c98643f36c03c518880fa226fa4975bbdd
fhernandez <fede.herz@gmail.com>
2019-12-13 15:22:03 -0300

    working

--- dqn2.py
+++ dqn2.py
@@ -72,7 +72,8 @@ class DQNAgent:
 
 
 def main():
-    env = gym.make('CartPole-v1')
+    # env = gym.make('CartPole-v1')
+    env = gym.make("MountainCar-v0")
     state_size = env.observation_space.shape[0]
     action_size = env.action_space.n
     agent = DQNAgent(state_size, action_size)
